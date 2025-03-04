7bc99441bb39585f597e1b9b1adb944742d9a57d
Nsiikak <nsikakebong98@gmail.com>
2025-03-04 15:40:53 +0100

    Chatbot implementation

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,
+    ChatModule,
   ],
   controllers: [],
   providers: [],
